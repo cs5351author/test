cb268ae4ff38933fd1495b183fbdd7080b0382f7
cs5351author <144524826+cs5351author@users.noreply.github.com>
2024-08-28 17:28:38 +0800

    Update mcr.cpp

--- mcr.cpp
+++ mcr.cpp
@@ -41,6 +41,7 @@ int main(){
 	   cout << "Tie!" << endl;
 
 	// show the game to console
+	//test
 	cout << game[0][0] << " " << game[0][1] << " " << game[0][2] << endl;
 	cout << game[1][0] << " " << game[1][1] << " " << game[1][2] << endl;
 	cout << game[2][0] << " " << game[2][1] << " " << game[2][2] << endl;
